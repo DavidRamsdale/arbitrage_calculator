824fae581ed75b7dd3263ee96e328711e1fbfd75
David Ramsdale <davidramsdale1991@gmail.com>
2018-12-03 20:35:19 +1100

    Added in bootstrap

--- app.js
+++ app.js
@@ -1,17 +1,66 @@
-let selector = document.querySelector("outcome_option").addEventListener("change", formSelector);
-// selector.addEventListener("change", formSelector);
-
 
 function formSelector () {
-    if (selector.value === 3){
-    document.getElementById("input_outcome").innerHTML = "Hello World";
+    if (selection.value === "2"){
+        document.getElementById("input_outcome").innerHTML = `Outcome 1: <input id="a" type="number"> Stake 1: <input id="stake1" type="text" disabled value=""> Return: <input type="text"> <br>
+        Outcome 2: <input id="b" type="number"> Stake 2: <input id="stake2" type="text" disabled value=""> Return: <input type="text"> <br>`;
+        }
+
+    if (selection.value === "3"){
+    document.getElementById("input_outcome").innerHTML = `Outcome 1: <input id="a" type="number"> Stake 1: <input id="stake1" type="text" disabled value=""> Return: <input type="text"> <br>
+    Outcome 2: <input id="b" type="number"> Stake 2: <input id="stake2" type="text" disabled value=""> Return: <input type="text"> <br>
+    Outcome 3: <input id="c" type="number"> Stake 3: <input id="stake3" type="text" disabled value=""> Return: <input type="text"> <br>`
     }
 
-    if (selector.value === 4){
-        document.getElementById("input_outcome").innerHTML = "Hello World";
-        }
 }
 
-function calculate () {
+function calculate (a,b,c) {
     
-}
+    let combine_margin = (1/a.value) + (1/b.value) + (1/c.value);
+    let total_stake = document.getElementById("total_stake").value;
+        if ( combine_margin <= 1){
+            document.getElementById("stake1").value = (total_stake * (1/a.value))/combine_margin;
+            document.getElementById("stake2").value = (total_stake * (1/b.value))/combine_margin;
+            document.getElementById("stake3").value = (total_stake * (1/c.value))/combine_margin;
+            document.getElementById("%profit").value = (1 - combine_margin)*100;
+            document.getElementById("profit").value = (1 - combine_margin)*total_stake;
+        }
+
+        else {
+            console.log("you dont");
+        }
+    }
+    // if (selection.value === "2"){
+    //     if ((1/a.value) + (1/b.value) <= 1){
+    //         console.log("You have an arb");
+    //     }
+
+    //     else {
+    //         console.log("you dont");
+    //     }
+    // }
+    
+    // if (selection.value === "3"){
+    //     if ((1/a.value) + (1/b.value) +(1/c.value) <= 1){
+    //         console.log("You have an arb");
+    //     }
+
+    //     else {
+    //         console.log("you dont");
+    //     }
+    // }
+
+    // if (selection.value === "4"){
+    //     if ((1/a.value) + (1/b.value) + (1/c.value) + (1/d.value)  <= 1){
+    //         console.log("You have an arb");
+    //     }
+
+    //     else {
+    //         console.log("you dont");
+    //     }
+    // }
+
+let button = document.getElementById("button");
+button.addEventListener("click", () => calculate(a,b,c));
+
+let selection = document.getElementById("outcome_option");
+selection.addEventListener("click", formSelector)
