fa4f9f17c0ebca1b1ad5dac2ddbba7ef0fce6606
David Ramsdale <davidramsdale1991@gmail.com>
2018-12-07 16:29:43 +1100

    Fixed the calc function to work for any arguments

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function formSelector () {
     if (selection.value === "3" && selector_switch === false){
         outcome = document.getElementById("outcomes");
-        outcome.innerHTML += `<p>Outcome 3: <input id="c" type="number"> Stake 3: <input id="stake3" type="text" disabled="" value=""> Return: <input type="text"> <br> <br> </p>`;
+        outcome.innerHTML += `<p>Outcome 3: <input class="number" type="number"> Stake 3: <input id="stake3" type="text" disabled="" value=""> Return: <input type="text"> <br> <br> </p>`;
         selector_switch = true;
     }
     if (selection.value === "2" && selector_switch === true){
@@ -14,31 +14,40 @@ function isDefined() {
 
 }
 
-function calculate (a,b,c) {
-    let combine_margin = (1/a.value) + (1/b.value) + (1/c.value);
+function calculate (numbers) {
+    let combine_margin = 0;
     let total_stake = document.getElementById("total_stake").value;
-        if ( combine_margin <= 1){
-            document.getElementById("stake1").value = (total_stake * (1/a.value))/combine_margin;
-            document.getElementById("stake2").value = (total_stake * (1/b.value))/combine_margin;
-            document.getElementById("stake3").value = (total_stake * (1/c.value))/combine_margin;
-            document.getElementById("%profit").value = (1 - combine_margin)*100;
-            document.getElementById("profit").value = (1 - combine_margin)*total_stake;
+    console.log(numbers);
+    numbers.forEach((item, index) => {
+        console.log(item);
+        combine_margin += (1/parseInt(item.value));
+    });
+    console.log(combine_margin);
+    if (combine_margin <= 1) {
+        numbers.forEach((item, index) => {
+            let stake = document.getElementById(`stake${index + 1}`).value = (total_stake * (1/parseInt(item.value)))/combine_margin;
+        });
+        document.getElementById("%profit").value = (1 - combine_margin)*100;
+        document.getElementById("profit").value = (1 - combine_margin)*total_stake;
+        header = document.getElementById("header");
+        header.style.background = "Green";
+        header.innerHTML = `<h1>Arb Calculator</h1><br><h2>Arb Exists </h2>`; 
 
-            header = document.getElementById("header");
-            header.style.background = "green";
-            header.innerHTML = `<h1>Arb Calculator</h1><br><h2> Arb Exists </h2>`;
-        }
-
-        else {
-            header = document.getElementById("header");
-            header.style.background = "red";
-            header.innerHTML = `<h1>Arb Calculator</h1><br><h2> No Arb Exists </h2>`;
-        }
+    } else {
+        header = document.getElementById("header");
+        header.style.background = "red";
+        header.innerHTML = `<h1>Arb Calculator</h1><br><h2> No Arb Exists </h2>`; 
     }
+
+}
     
 
 let button = document.getElementById("button");
-button.addEventListener("click", () => calculate(a,b,c));
+button.addEventListener("click", () => {
+    let numbers = document.querySelectorAll(".number");
+    numbers = Array.from(numbers);
+    calculate(numbers);
+});
 
 let selection = document.getElementById("outcome_option");
 selection.addEventListener("click", formSelector)
